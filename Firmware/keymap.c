d812fec4c45b274d2b1e9acac775f5e80999032a
Tei-Pancake <112350302+Tei-Pancake@users.noreply.github.com>
2026-06-27 19:48:57 -0400

    Delete Firmware directory

--- Firmware/keymap.c
+++
@@ -1,9 +0,0 @@
-#include QMK_KEYBOARD_H
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_directional_3key(
-        LGUI(KC_D), // Tecla 1: Minimiza todo (Simulador de Trabajo)
-        LCTL(KC_W), // Tecla 2: Cierra pestaña actual (Limpieza rápida)
-        LGUI(KC_L)  // Tecla 3: Bloqueo de seguridad
-    )
-};
