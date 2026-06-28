3e39202fa8790075b41980e756489d0ba8a69e50
Tei-Pancake <112350302+Tei-Pancake@users.noreply.github.com>
2026-06-27 20:15:43 -0400

    Add files via upload

--- Production/main.py
+++ Production/main.py
@@ -1,4 +1,5 @@
 import board
+import busio  # Necesario para el bus I2C
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
@@ -13,22 +14,23 @@ keyboard.row_pins = (board.D0,)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 # 3. Definir tus funciones de "Modo Ninja"
-# Tecla 1: Win+D (Minimizar todo)
-# Tecla 2: Ctrl+W (Cerrar pestaña)
-# Tecla 3: Win+L (Bloquear PC)
 keyboard.keymap = [
     [KC.LGUI(KC.D), KC.LCTL(KC.W), KC.LGUI(KC.L)]
 ]
 
-# 4. Configurar la pantalla OLED
+# 4. Configurar el bus I2C para la pantalla OLED (Pines D4 y D5)
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# 5. Configurar la pantalla OLED pasando el bus i2c
 oled_ext = Oled(
     OledDisplayMode.TXT,
     oled_reaction_type=OledReactionType.LAYER,
+    i2c=i2c,  # Aquí conectamos el bus que definimos arriba
     width=128,
     height=32,
 )
 keyboard.extensions.append(oled_ext)
 
-# 5. ¡Arrancar el Keyboardinator!
+# 6. ¡Arrancar el Keyboardinator!
 if __name__ == '__main__':
     keyboard.go()
